a0d3173e912e75933d49c3eda3680b63b6383347
ows3090 <ows3090@naver.com>
2024-04-10 20:59:45 +0900

    [chore] #267 Remove PartyMemberCreate UI and previous Detail UI

--- feature/partymember/src/main/java/com/cmc/curtaincall/feature/partymember/create/PartyMemberCreateSideEffect.kt
+++
@@ -1,7 +0,0 @@
-package com.cmc.curtaincall.feature.partymember.create
-
-import com.cmc.curtaincall.core.base.BaseSideEffect
-
-sealed class PartyMemberCreateSideEffect : BaseSideEffect {
-    object SuccessUpload : PartyMemberCreateSideEffect()
-}
--- feature/partymember/src/main/java/com/cmc/curtaincall/feature/partymember/test/PartyMemberDetail2SideEffect.kt
+++
@@ -1,8 +0,0 @@
-package com.cmc.curtaincall.feature.partymember.test
-
-import com.cmc.curtaincall.core.base.BaseSideEffect
-
-sealed class PartyMemberDetail2SideEffect : BaseSideEffect {
-    object SuccessDelete : PartyMemberDetail2SideEffect()
-    object SuccessParticipation : PartyMemberDetail2SideEffect()
-}
